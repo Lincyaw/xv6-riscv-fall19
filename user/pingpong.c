7019c6c237a7b7df7123e19c3b731fcaf7abc59b
Lincyaw <814750204@qq.com>
2020-10-26 17:10:56 +0800

    Finish pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,27 +1,24 @@
 #include "kernel/types.h"
 #include "user/user.h"
+
 int main(int argc, char *argv[])
 {
-    int  pid;
-    int parent[2], child[2];
-    pipe(parent);
-    pipe(child);
+    int parent_fd[2], child_fd[2];
+    pipe(parent_fd);
+    pipe(child_fd);
     char buf[64];
 
-    if ((pid=fork())==0)
+    if (!fork())
     {
-        // Child
-        read(parent[0], buf, 4);
+        read(parent_fd[0], buf, strlen("ping"));
         printf("%d: received %s\n", getpid(), buf);
-        write(child[1], "pong", strlen("pong"));
-       
+        write(child_fd[1], "pong", strlen("pong"));
     }
     else
     {
-        // Parent
-        write(parent[1], "ping", strlen("ping"));
-        read(child[0], buf, 4);
-        printf("%d: received %s. childpid = %d\n", getpid(), buf, pid);
+        write(parent_fd[1], "ping", strlen("ping"));
+        read(child_fd[0], buf, strlen("pong"));
+        printf("%d: received %s\n", getpid(), buf);
     }
 
     exit();
