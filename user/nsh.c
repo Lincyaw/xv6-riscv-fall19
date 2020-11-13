7951c62f9607f80e538c876905d5e0c86e3aebb3
lincyaw <814750204@qq.com>
2020-11-13 16:24:22 +0800

    add some sh comments

--- user/nsh.c
+++ user/nsh.c
@@ -3,14 +3,16 @@
 #include "user/user.h"
 #include "kernel/fcntl.h"
 
+// 输入命令长度最大为1024
+char cmd_buf[1024];
+char *left, *right;
+
 inline int print(int fd, char *str)
 {
     return write(fd, str, strlen(str));
 }
 
-// [in-place]
-// replace the left side "|" with "\0"
-// then return the rest of the string or NULL
+// 把'|'换成终结符'\0',返回剩余的字符
 char *simple_tok(char *p, char d)
 {
     while (*p != '\0' && *p != d)
@@ -43,9 +45,6 @@ void redirect(int k, int pd[])
     close(pd[0]);
     close(pd[1]);
 }
-
-char cmd_buf[1024];
-char *a, *n;
 
 void handle(char *cmd)
 {
@@ -121,31 +120,35 @@ void handle(char *cmd)
         exec(pass2[0], pass2);
     }
 }
-
+/// 递归函数, 递归处理命令
 void handle_cmd()
 {
-    if (a)
+    if (left)
     {
         int pd[2];
         pipe(pd);
         // int parent_pid = getpid();
         // fprintf(2, "pid: %d, cmd: %s\n", parent_pid, a);
 
-        if (!fork())
+        if (!fork()) //parent
         {
             // fprintf(2, "%d -> %d source\n", parent_pid, getpid());
-            if (n)
+            // 把pd写端重定向到标准输出
+            if (right)
                 redirect(1, pd);
-            handle(a);
+            handle(left);
         }
-        else if (!fork())
+        else if (!fork()) //子进程中，再fork一下
         {
             // fprintf(2, "%d -> %d sink\n", parent_pid, getpid());
-            if (n)
+            if (right)
             {
+                // 父进程中将写端给到标准输出了,这里的子进程将父进程写的东西读进来
+                // 所以将读端给标准输入
                 redirect(0, pd);
-                a = n;
-                n = simple_tok(a, '|');
+                left = right;
+                right = simple_tok(left, '|');
+                // 递归调用
                 handle_cmd();
             }
         }
@@ -166,11 +169,12 @@ int main(int argc, char *argv[])
     {
         print(1, "@");
         memset(cmd_buf, 0, 1024);
+        // 接受命令
         gets(cmd_buf, 1024);
-
+        // 刚刚初始化为0,如果还是0,则说明没有读进来
         if (cmd_buf[0] == 0) // EOF
             exit(0);
-
+        // 把回车换成字符终结符
         *strchr(cmd_buf, '\n') = '\0';
 
         if (fork())
@@ -179,8 +183,8 @@ int main(int argc, char *argv[])
         }
         else
         {
-            a = cmd_buf;
-            n = simple_tok(a, '|');
+            left = cmd_buf;
+            right = simple_tok(left, '|');
             handle_cmd();
         }
     }
